0a6addc25435eb9b817e0cf765673afe331e8d7a
al-abir-anik <alabiranik56@gmail.com>
2024-09-25 07:52:24 +0600

    transaction history functionality added

--- javascript/features.js
+++ javascript/features.js
@@ -1,60 +1,71 @@
 
+// Input Validate, handle Donation and Modal Open
+function handleDonation(inputId, modalId, balanceId, reliefName) {
+  const donationInput = document.getElementById(inputId).value;
+  const donationAmount = parseFloat(donationInput);
 
-// function handleDonation(inputId, modalId) {
-//     const donationAmount = document.getElementById(inputId).value;
-//     const myBalance = document.getElementById('my-balance');
+  const myBalanceText = document.getElementById("my-balance").innerText;
+  const myBalance = parseFloat(myBalanceText.match(/\d+/)[0]);
 
-//     if (donationAmount == '') {
-//         alert('Please Enter a Valid Number');        
-//         return;
-//     }
-//     if(donationAmount <= 0) {
-//         alert('Please Enter a Valid Number. Donation Amount Can not be 0');
-//         return;
-//     }
-//     if(donationAmount > myBalance){
-//         alert('Your Account does not have enough balance');
-//         return;
-//     }
-    
-//     document.getElementById(modalId).modalNoakhali();   
-// }
+  if (donationInput === "" || isNaN(donationAmount)) {
+    alert("Please Enter a Valid Number");
+    return 0;
+  }
+  if (donationAmount <= 0) {
+    alert("Please Enter a Valid Number. Donation Amount Can not be 0");
+    return 0;
+  }
+  if (donationAmount > myBalance) {
+    alert("You Dont Have Enough Balance to Donate");
+    return 0;
+  }
 
+  const totalDonationText = document.getElementById(balanceId).innerText;
+  const totalDonation = parseFloat(totalDonationText.match(/\d+/)[0]);
+  const newTotalDonation = totalDonation + donationAmount;
+  document.getElementById(balanceId).innerText = newTotalDonation + " BDT";
 
+  const myNewBalance = myBalance - donationAmount;
+  document.getElementById("my-balance").innerText = myNewBalance + " BDT";
 
-function handleDonation(inputId, modalId, balanceId){
-    const donationInput = document.getElementById(inputId).value;
-    const donationAmount = parseFloat(donationInput);
+  const modal = document.getElementById(modalId);
+  modal.showModal();
 
-    const myBalanceText = document.getElementById('my-balance').innerText;
-    const myBalance = parseFloat(myBalanceText.match(/\d+/)[0]);
-
-    if (donationInput === '' || isNaN(donationAmount)) {
-        alert('Please Enter a Valid Number');        
-        return 0;
-    }
-    if (donationAmount <= 0) {
-        alert('Please Enter a Valid Number. Donation Amount Can not be 0');
-        return 0;
-    }
-    if (donationAmount > myBalance) {
-        alert('You Dont Have Enough Balance to Donate');
-        return 0;
-    }
-
-    const totalDonationText = document.getElementById(balanceId).innerText;
-    const totalDonation = parseFloat(totalDonationText.match(/\d+/)[0]);
-    const newTotalDonation = totalDonation + donationAmount ;
-    document.getElementById(balanceId).innerText = newTotalDonation + ' BDT';
-
-    const myNewBalance = myBalance - donationAmount ;
-    document.getElementById('my-balance').innerText = myNewBalance + ' BDT';
-    
-    const modal = document.getElementById(modalId);
-    modal.showModal();
+  const date = new Date();
+  const historyDiv = document.createElement('div');
+  historyDiv.classList.add('w-full', 'h-auto', 'p-8', 'mb-6', 'rounded-2xl', 'bg-white', 'border', 'border-[#111111]/10');
+  historyDiv.innerHTML = `
+    <h3 class="mb-3 text-xl font-bold text-[#111111]">${donationInput} Taka is Donated for ${reliefName}, Bangladesh</h3>
+    <p class="text-base font-light text-[#111111]/70">Date : ${date.toString()}</p>
+  `
+  document.getElementById('history-section').prepend(historyDiv);
 }
 
 
-// let myBalanceText = document.getElementById('my-balance').innerText;
-// let myBalance = parseFloat(myBalanceText.match(/\d+/)[0]);
+// function transactionHistory(para,params) {
+//   const donationInput = document.getElementById(para).value;
+//   const date = new Date();
 
+//   const historyDiv = document.createElement('div');
+//   historyDiv.classList.add('w-full', 'h-auto', 'p-8', 'mb-6', 'rounded-2xl', 'bg-white', 'border', 'border-[#111111]/10');
+//   historyDiv.innerHTML = `
+//     <h3 class="mb-3 text-xl font-bold text-[#111111]">${donationInput} Taka is Donated for ${params}, Bangladesh</h3>
+//     <p class="text-base font-light text-[#111111]/70">Date : ${date.toString()}</p>
+//   `
+//   document.getElementById('history-section').prepend(historyDiv);
+  
+// }
+
+
+// Donation and History Section Change
+function showSection(sectionId) {
+  document.getElementById("donation-section").classList.add("hidden");
+  document.getElementById("history-section").classList.add("hidden");
+  document.getElementById(sectionId).classList.remove("hidden");
+
+  // document.getElementById("btn-donation-section").classList.add("bg-transparent");
+  // document.getElementById("btn-history-section").classList.add("bg-transparent");
+  // document.getElementById(sectionId).classList.remove("bg-[#B4F461]");
+
+
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,22 @@
 
-// Input Validate and Modal Open
+// Input Validate, handle Donation and Modal Open
     document.getElementById('btn-donateNow-noakhali').addEventListener('click',function () {
-        handleDonation('input-amount-noakhali', 'my_modal_noakhali', 'total-donation-noakhali');
+        handleDonation('input-amount-noakhali', 'my_modal_noakhali', 'total-donation-noakhali', 'Flood Relief in Noakhali');
     });
 
     document.getElementById('btn-donateNow-feni').addEventListener('click',function () {
-        handleDonation('input-amount-feni', 'my_modal_feni', 'total-donation-feni');
+        handleDonation('input-amount-feni', 'my_modal_feni', 'total-donation-feni', 'Flood Relief in Feni');
     });
 
     document.getElementById('btn-donateNow-quota').addEventListener('click',function () {
-        handleDonation('input-amount-quota', 'my_modal_quota', 'total-donation-quota');
+        handleDonation('input-amount-quota', 'my_modal_quota', 'total-donation-quota', 'Aid for Injured in the Quota Movement');
     });
+
+
+// Donation and History Section Change
+    document.getElementById('btn-history-section').addEventListener('click',function () {
+        showSection('history-section');
+    })  
+    document.getElementById('btn-donation-section').addEventListener('click',function () {
+        showSection('donation-section');
+    })  
